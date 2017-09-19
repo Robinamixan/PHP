509a4ba78ea2a0de84b28dac9cf8f443be57181f
Robinam <robinamixan@mail.ru>
2017-09-19 09:36:20 +0300

    dd

---
+++ hello.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>register</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/">
+    </head>
+    <body>
+        <header></header>
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="Registration.html">Регистрация</a>
+                </li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+
+        <?php
+            echo "hello";
+        ?>
+
+        <footer></footer>
+    </body>
+</html>
